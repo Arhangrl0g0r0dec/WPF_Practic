f26bc7c95389d9a6bb7bafcf290d887460af0d89
Arhangrl0g0rodec <Arhangel0g0r0d@mil.ru>
2021-11-15 19:04:06 +0300

    Создано окно входа, кабинет лаборанта и функционал для лаборанта.

---
+++ WPF_Practic/WPF_Practic/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Practic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Private_Cabinet private_Cabinet = new Private_Cabinet();
+            private_Cabinet.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WPF_Practic/WPF_Practic/Private_Cabinet.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Practic
+{
+    /// <summary>
+    /// Логика взаимодействия для Private_Cabinet.xaml
+    /// </summary>
+    public partial class Private_Cabinet : Window
+    {
+        public Private_Cabinet()
+        {
+            InitializeComponent();
+        }
+
+        private void Bio_Click(object sender, RoutedEventArgs e)
+        {
+            Biomat biomat = new Biomat();
+            biomat.Owner = this;
+            biomat.Show();
+        }
+
+        private void DoOtchet_Click(object sender, RoutedEventArgs e)
+        {
+            Otchet otchet = new Otchet();
+            otchet.Owner = this;
+            otchet.Show();
+        }
+    }
+}
